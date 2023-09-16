4e70e584ef52540f2e2654b13e74f2d945dd763a
EvanSchulte <etschulte04@gmail.com>
2023-09-16 17:39:28 -0400

    Refactored the code for creating a book object and added comments

--- LibraryDesktopApp/src/Main.java
+++ LibraryDesktopApp/src/Main.java
@@ -12,61 +12,76 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
- 
-
- 
-
- 
-
 class Book {
-
-	/*
-
-	String Authors;
-
-	int OriginalPublicationYear;
-
-	int Isbn;
-
-	int BookID;
-
-	String Title;
-
-	Double AverageRating;
-
-	 */
-
-	ArrayList<String> bookId = new ArrayList<String>();
-
-	ArrayList<String> originalPublicationYear = new ArrayList<String>();
-
-	ArrayList<String> Isbn = new ArrayList<String>();
-
-	ArrayList<String> Title = new ArrayList<String>();
-
-	ArrayList<String> AverageRating = new ArrayList<String>();
-
 	
+    private String bookId;
+    private String isbn;
+    private String author;
+    private String title;
+    private String rating;
+    private String publishingYear;
+    
+    // makes a book object
+    public Book(String bookId, String isbn, String author, String title, String rating, String publishingYear) {
+        this.bookId = bookId;
+        this.isbn = isbn;
+        this.author = author;
+        this.title = title;
+        this.rating = rating;
+        this.publishingYear = publishingYear;
+    }
+    
+    // gets the book's ID
+    public String getBookId() {
+    	return this.bookId;
+    }
+    
+    // gets the book's Isbn
+    public String getBookIsbn() {
+    	return this.isbn;
+    }
+    
+    // gets the book's author
+    public String getBookAuthor() {
+    	return this.author;
+    }
+    
+    // gets the book's title
+    public String getBookTitle() {
+    	return this.title;
+    }
+    
+    // gets the book's rating
+    public String getBookRating() {
+    	return this.rating;
+    }
+    
+    // gets the book's publishing year
+    public String getBookPubYear() {
+    	return this.publishingYear;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Book ID: " + bookId +
+               ", ISBN: " + isbn +
+               ", Author: " + author +
+               ", Title: " + title +
+               ", Rating: " + rating +
+               ", Publishing Year: " + publishingYear;
+    }
 
 }
 
-
- 
-
 public class Main {
-
- 
 
 	public static void main(String[] args) {
 		
 
-		
-
-		
-
 //Reading File________________________________________________________________________________________________________________
 
-		String Path = "src/books.csv";
+		String Path = "src/books(1).csv";
 
 		String line = "";
 
@@ -98,37 +113,20 @@ public class Main {
 
 				
 
-				//Display all fields
-
-					/*
-
-				for (String[] array : books) {
-
-						
-
-			            for (String element : array) {
-
-			                System.out.print(element + "   ");
-
-			            }
-
-			            System.out.println(); // Print a newline after each array
-
-			        }
-
-				*/
-
 //________________________________________________________________________________________________________________
 
 					//Creating book Object
 
-				Book BookObj = new Book();
+				//Book BookObj = new Book();
+				
 
 				
 
 			
 
 				boolean skipTheFirstIteration = true;
+				
+				ArrayList<Book> Books = new ArrayList<>();
 
 				
 
@@ -150,59 +148,32 @@ public class Main {
 
 					}
 					
-
-					
-
-					
-/*
-					BookObj.bookId.add(String.valueOf(array[0]));
-
-					//BookObj.Title.add(array[10]);
-
-					//Unable to parse some input, as some contains letters and other
-
-					BookObj.Isbn.add(String.valueOf(array[5]));
-
-					BookObj.originalPublicationYear.add(String.valueOf(array[8]));
-
-					BookObj.AverageRating.add(String.valueOf(array[13]));
-*/
-					
-
+					// Gets the value at the specified index in the array
+					String bookAuthor = String.valueOf(array[7]);
+					String bookTitle = String.valueOf(array[10]);
 					int bookIdNum = Integer.parseInt(array[0]);
-					int bookIsbnNum = Integer.parseInt(array[5]);
+					String bookIsbnNum = String.valueOf(array[5]);
 					double originalPublishingYear = Double.parseDouble(array[8]);
-					double averageRating = Double.parseDouble(array[13]);
+					double averageRating = Double.parseDouble(array[12]);
 					
+					// Turns non-string variables to strings
 					String bookId = Integer.toString(bookIdNum);
-					String bookIsbn = Integer.toString(bookIsbnNum);
 					String origPubYear = Double.toString(originalPublishingYear);
 					String avgRating = Double.toString(averageRating);
 					
-					
-					
-					System.out.println(makeBookObj(bookId, bookIsbn, origPubYear, avgRating));
-					
-					
-					//System.out.println("First element of the array: " + bookId);
-					//System.out.println("Book Isbn is: " + bookIsbn);
-					//System.out.println("The original publicaton year is: " + origPubYear);
-					//System.out.println("The average rating of this book is: " + avgRating + " stars.");
-					//System.out.println("");
-					
-					
-
-					
+					// Creates a book object in the Book arrayList
+					Book book = new Book(bookId, bookIsbnNum, bookAuthor, bookTitle, avgRating, origPubYear);
+		            Books.add(book);
 
 				}
 				
+				// Testing looking up the title of a book in the Books arraylist
+				Book bookIndex = Books.get(88);
+				String bookTitleofFirstBook = bookIndex.getBookTitle();
+				System.out.println(bookIndex);
+				System.out.println(bookTitleofFirstBook);
 				
 				
-
-				//System.out.println(BookObj.bookId);
-
-				
-
 			} catch (FileNotFoundException e) {
 
 				e.printStackTrace();
@@ -213,23 +184,8 @@ public class Main {
 
 			}
 
-			
-
-			
-
 	}
 
-	private static ArrayList<String> makeBookObj(String bookId, String bookIsbn, String origPubYear, String avgRating) {
-		ArrayList<String> Book = new ArrayList<String>();
-		
-		Book.add(bookId);
-		Book.add(bookIsbn);
-		Book.add(origPubYear);
-		Book.add(avgRating);
-		
-		return Book;
-		
-	}
 }
 
 
