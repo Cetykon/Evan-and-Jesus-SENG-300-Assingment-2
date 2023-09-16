ba47dda7731405474bcd9a695a794925c2885808
Cetykon <jesusaguilar92348@gmail.com>
2023-09-16 14:03:14 -0400

    Updated CSV file and selection sort works to sort linked list
    I formatted the selection sort to take in linked list and change some of the logic with linked list methods.
    Their was a year which was -720, I change it to 1720 in the csv file.

--- LibraryDesktopApp/src/TestingBranch.java
+++ LibraryDesktopApp/src/TestingBranch.java
@@ -1,9 +1,10 @@
 
 import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Arrays;
 
 
@@ -19,103 +20,138 @@ class Book {
 	String Title;
 	Double AverageRating;
 	 */
-	ArrayList<Integer> bookId = new ArrayList<Integer>();
-	ArrayList<Integer> originalPublicationYear = new ArrayList<Integer>();
-	ArrayList<Integer> Isbn = new ArrayList<Integer>();
-	ArrayList<String> Title = new ArrayList<String>();
-	ArrayList<Double> AverageRating = new ArrayList<Double>();
+	LinkedList<Integer> bookId = new LinkedList<Integer>();
+	LinkedList<Integer> originalPublicationYear = new LinkedList<Integer>();
+	LinkedList<String> Isbn = new LinkedList<String>();
+	LinkedList<String> Title = new LinkedList<String>();
+	LinkedList<Double> AverageRating = new LinkedList<Double>();
 	
-
-
-public void readCSV() {
-	
-	//Reading File________________________________________________________________________________________________________________
-			String Path = "src/books.csv";
-			String line = "";
-			
-				BufferedReader Reader;
-				
-				try {
-					Reader = new BufferedReader(new FileReader(Path));
-					
-					ArrayList<String[]> books = new ArrayList<String[]>();
-					
-					while((line = Reader.readLine()) != null) {
-						
-						books.add(line.split(","));
-					
-					}
-					
-					Reader.close();
-					
-					//Display all fields
-						/*
-					for (String[] array : books) {
-							
-				            for (String element : array) {
-				                System.out.print(element + "   ");
-				            }
-				            System.out.println(); // Print a newline after each array
-				        }
-					*/
-	//Storing fields into respective arraylist________________________________________________________________________________
-						//Creating book Object
-					Book BookObjMethod = new Book();
-					
-				
-					boolean skipTheFirstIteration = true;
-					
-					//Iterating through data collected
-					for (String[] array : books) {
-						
-						//This if statement allows skips the first iteration of the loop 
-						if (skipTheFirstIteration) {
-							
-							skipTheFirstIteration = false;
-							continue;
-						}
-						
-						
-						BookObjMethod.bookId.add(Integer.parseInt(array[0]));
-						BookObjMethod.Title.add(array[10]);
-						//Unable to parse some input, as some contains letters and other 
-						//problem starts after index 7
-						//BookObj.Isbn.add(Integer.parseInt(array[5]));
-						//BookObj.originalPublicationYear.add(Integer.parseInt(array[8]));
-						//BookObj.AverageRating.add(Double.parseDouble(array[12]));
-						
-						//Printing fields for testing
-						String firstElement = array[3];
-						System.out.println("First element of the array: " + firstElement);
-						}
-					
-									
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-		}
 		
 }
+
+//SelectionSort_________________________________________________________________________________________________________
+class modifySelectionSort {
+
+	/**
+	 *@author downey
+	 *
+		/**
+		 * Swaps the elements at indexes i and j.
+		 */
+		public static void swapElements(LinkedList<Integer> originalPublicationYear, int i, int j) {
+			int temp = originalPublicationYear.get(i);
+			originalPublicationYear.set(i,originalPublicationYear.get(j));
+			originalPublicationYear.set(j,temp);
+			
+		}
+		/**
+		 * Finds the index of the lowest value
+		 * between indices low and high (inclusive).
+		 */
+		public static int indexLowest(LinkedList<Integer> originalPublicationYear, int start) {
+			int lowIndex = start;
+			for (int i = start; i < originalPublicationYear.size(); i++) {
+				if (originalPublicationYear.get(i) < originalPublicationYear.get(lowIndex)) {
+					lowIndex = i;
+				}
+			}
+			return lowIndex;
+		}
+
+		/**
+		 * Sorts the cards (in place) using selection sort.
+		 * @return 
+		 */
+		public LinkedList<Integer> selectionSort(LinkedList<Integer> originalPublicationYear) {
+			for (int i = 0; i < originalPublicationYear.size(); i++) {
+				int j = indexLowest(originalPublicationYear, i);
+				swapElements(originalPublicationYear, i, j);
+				//Author Swap
+				//Isbn
+				//BookID
+				//Title
+				//AverageRating
+				
+			}
+			return originalPublicationYear;
+		}
+
+	
+}
+
 
 
 public class TestingBranch {
 
 	public static void main(String[] args) {
 	
+//Reading File________________________________________________________________________________________________________________
+	String Path = "src/books.csv";
+	String line = "";
+	
+		BufferedReader Reader;
 		
-		Book BookObjMain = new Book();
+		try {
+			Reader = new BufferedReader(new FileReader(Path));
+			
+			LinkedList<String[]> books = new LinkedList<String[]>();
+			
+			while((line = Reader.readLine()) != null) {
+				
+				books.add(line.split(","));
+			
+			}
+			
+			Reader.close();
+			
+			//Display all fields
+				/*
+			for (String[] array : books) {
+					
+		            for (String element : array) {
+		                System.out.print(element + "   ");
+		            }
+		            System.out.println(); // Print a newline after each array
+		        }
+			*/
+//Storing fields into respective arraylist________________________________________________________________________________
+				//Creating book Object
+			Book BookObjMethod = new Book();
+			
 		
-		BookObjMain.readCSV();
+			boolean skipTheFirstIteration = true;
+			
+			//Iterating through data collected
+			for (String[] array : books) {
+				
+				//This if statement allows skips the first iteration of the loop 
+				if (skipTheFirstIteration) {
+					
+					skipTheFirstIteration = false;
+					continue;
+				}
+				
+				
+				BookObjMethod.bookId.add(Integer.parseInt(array[0]));
+				BookObjMethod.Title.add(array[10]);
+				BookObjMethod.Isbn.add(array[5]);
+				BookObjMethod.originalPublicationYear.add(Integer.parseInt(array[8]));
+				BookObjMethod.AverageRating.add(Double.parseDouble(array[12]));
+				
+				//Printing fields for testing
+				//String firstElement = array[8];
+				//System.out.println(firstElement);
+				}
+			
+
 //________________________________________________________________________________________________________________
+			modifySelectionSort publicYear = new modifySelectionSort();		
+			
+			BookObjMethod.originalPublicationYear = publicYear.selectionSort(BookObjMethod.originalPublicationYear);
+				
+	
 					
-					
-					
-					
-					
-					
-					
+			System.out.println(BookObjMethod.originalPublicationYear);
 					
 			
 					
@@ -125,8 +161,12 @@ public class TestingBranch {
 					
 					
 				
-				//System.out.println(BookObj.bookId);
 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 			
 			
 	}
